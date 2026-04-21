89ccfce7d23797f24cf1325d28dc7b735b4a05ae
Matteus <titansacustikos@gmail.com>
2026-04-21 08:14:12 -0400

    fix: PORT 10000 para Render

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const path    = require('path');
 const { google } = require('googleapis');
 
 const app  = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
